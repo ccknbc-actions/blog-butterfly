3928cb2b06a4cb72b865e48560f1f8b6115f52db
CCKNBC <zekangsun@outlook.com>
2020-08-08 15:07:42 +0800

    2020/08/08 周六 15:07:40.94: Updated By CCKNBC Local

---
+++ source/timeline/index.md
@@ -0,0 +1,25 @@
+---
+title: 时间线
+date: 2020-08-08 14:55:19
+type: timeline
+aside: false
+comments: false
+---
+
+{% timeline 碎碎念 %}
+
+{% timenode 2020-08-06 [连接测试](https://xiaokang.me) %}
+
+今天是2020-08-06
+
+{% endtimenode %}
+
+{% timenode 2020-08-05 [连接测试](https://xiaokang.me) %}
+
+今天是2020-08-05
+
+{% endtimenode %}
+
+{% endtimeline %}
+
+{% link 如何参与项目, http://localhost:4000/contributors/, https://cdn.jsdelivr.net/gh/volantis-x/cdn-org/blog/Logo-NavBar@3x.png %}
--- themes/butterfly/source/js/ccknbc.js
+++ themes/butterfly/source/js/ccknbc.js
@@ -59,3 +59,23 @@ document.addEventListener('visibilitychange', function() {
         }, 2000);
     }
 });
+
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+    "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " menus-closed";
+}
+
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+    "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " menus-closed";
+}
