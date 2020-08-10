edffc0f5c838e27c2695af92e9833790ebfc7f7c
CCKNBC <zekangsun@outlook.com>
2020-08-10 13:24:17 +0800

    2020/08/10 周一 13:24:16.37: Updated By CCKNBC Local

--- themes/butterfly/source/js/ccknbc.js
+++ themes/butterfly/source/js/ccknbc.js
@@ -60,21 +60,6 @@ document.addEventListener('visibilitychange', function() {
     }
 });
 
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-    "menus_item_child"
-);
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " menus-closed";
-}
-
-var full_page = document.getElementsByClassName("full_page");
-if (full_page.length != 0) {
-  full_page[0].style.background = "transparent";
-}
-
 var card_category_list = document.getElementsByClassName(
     "card-category-list child"
 );
@@ -84,16 +69,25 @@ function toggle(t) {
     if (display == "none") {
         t.parentElement.nextSibling.style.display = "block";
         t.parentElement.nextSibling.style.height = "100%";
-        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
+        t.className = t.className.replace("fas-chevron-up", "fas-chevron-down");
     } else {
         t.parentElement.nextSibling.style.display = "none";
-        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
+        t.className = t.className.replace("fas-chevron-down", "fas-chevron-up");
     }
 }
 
 for (var i = 0; i < card_category_list.length; i++) {
     card_category_list[i].style.display = "none";
     card_category_list[i].style.transition = "all 1s";
-    card_category_list[i].previousSibling.innerHTML +=
-        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>';
+    card_category_list[i].previousSibling.innerHTML += '<i class="fas fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>';
+}
+
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+    "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " menus-closed";
 }
