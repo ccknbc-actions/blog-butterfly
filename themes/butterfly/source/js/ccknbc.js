7b8a47acffd9dc4c74a3fcee234b9ab985ca57ac
CCKNBC <zekangsun@outlook.com>
2020-08-15 21:55:00 +0800

    2020/08/15 周六 21:54:59.21: Updated By CCKNBC Local

--- themes/butterfly/source/js/ccknbc.js
+++ themes/butterfly/source/js/ccknbc.js
@@ -1,9 +1,3 @@
-function myrefresh()
-{
-window.location.reload();
-}
-setTimeout('myrefresh()',300000); //指定1秒刷新一次
-
 // 设置页脚博主会动的心
 $(document).ready(function(e){
     $('.copyright').html('©2020 <i style="color:#49B1F5;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> CC康纳百川');
@@ -65,3 +59,40 @@ document.addEventListener('visibilitychange', function() {
         }, 2000);
     }
 });
+
+/* 控制台 */
+(function() {
+	if (window.console && window.console.log) {
+		console.log(
+			'\n %c CC康纳百川 %c https://ccknbc.github.io \n',
+			'color:#FFFFFB;background:#ffa628;padding:5px 0;border-radius:.5rem 0 0 .5rem;',
+			'background: #efefef;padding:5px 0 5px;border-radius:0 .5rem .5rem 0;'
+		)
+		console.log(
+			`%c页面加载消耗了 ${(
+				Math.round(performance.now() * 100) /
+				100 /
+				1000
+			).toFixed(2)}s`,
+			'background: #fff;color: #333;text-shadow: 0 0 2px #eee, 0 0 3px #eee, 0 0 3px #eee, 0 0 2px #eee, 0 0 3px #eee;'
+		)
+		if (!localStorage.getItem('access')) {
+			localStorage.setItem('access', new Date().getTime())
+		}
+		let _access = new Date(parseInt(localStorage.getItem('access')))
+		let access = `${_access.getFullYear()}年${_access.getMonth() +
+			1}月${_access.getDate()}日`
+		let i = 0
+		if (!localStorage.getItem('hit')) {
+			localStorage.setItem('hit', 0)
+		} else {
+			i = parseInt(localStorage.getItem('hit'))
+		}
+		localStorage.setItem('hit', ++i)
+		console.log(
+			`这是你自 ${access} 以来第 ${i} 次在本站打开控制台，你想知道什么秘密嘛~`
+		)
+		const img = `https://cdn.jsdelivr.net/gh/Tomotoes/images/console/${i % 5}.jpg`
+		console.log('%c ', '\n  background:url('.concat(img, ') no-repeat center;\n  background-size:200px;\n  margin:5px 0;\n  padding:0 0 162px 200px;\n  border-radius:10px;\n  overflow:hidden;\n  '))
+	}
+})()
