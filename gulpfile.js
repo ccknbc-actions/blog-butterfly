1a46645aa00b204d03ce7012e9e19a944f50010d
CCKNBC <ccknbc@qq.com>
2020-11-23 20:52:54 +0800

    2020/11/23 周一 20:52:53.97: Updated By CCKNBC Local

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,12 @@ var gulp = require('gulp')
 var cleanCSS = require('gulp-clean-css')
 var htmlmin = require('gulp-htmlmin')
 var htmlclean = require('gulp-htmlclean')
-// var imagemin = require('gulp-imagemin')
-// tester (如果使用tester,把下面4行前面的//去掉)
 var uglifyjs = require('terser')
 var composer = require('gulp-uglify/composer')
 var pump = require('pump')
 var minify = composer(uglifyjs, console)
+var workbox = require("workbox-build");
 
-// babel (如果不是使用bebel,把下面兩行註釋掉)
-// var uglify = require('gulp-uglify')
-// var babel = require('gulp-babel')
-
-// minify js - babel（ 如果不是使用bebel,把下面註釋掉）
-// gulp.task('compress', () =>
-//   gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-//     .pipe(babel({
-//       presets: ['@babel/preset-env']
-//     }))
-//     .pipe(uglify().on('error', function (e) {
-//       console.log(e)
-//     }))
-//     .pipe(gulp.dest('./public'))
-// )
-
-// minify js - tester (如果使用tester,把下面前面的//去掉)
 gulp.task('compress', function (cb) {
   var options = {}
   pump([
@@ -37,14 +19,12 @@ gulp.task('compress', function (cb) {
   )
 })
 
-// css
 gulp.task('minify-css', () => {
   return gulp.src('./public/**/*.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('./public'))
 })
 
-// 壓縮 public 目錄內 html
 gulp.task('minify-html', () => {
   return gulp.src('./public/**/*.html')
     .pipe(htmlclean())
@@ -62,20 +42,22 @@ gulp.task('minify-html', () => {
     .pipe(gulp.dest('./public'))
 })
 
-// 壓縮 public/uploads 目錄內圖片
-// gulp.task('minify-images', async () => {
-//   gulp.src('./public/img/**/*.*')
-//     .pipe(imagemin({
-//       optimizationLevel: 5, // 類型：Number  預設：3  取值範圍：0-7（優化等級）
-//       progressive: true, // 類型：Boolean 預設：false 無失真壓縮jpg圖片
-//       interlaced: false, // 類型：Boolean 預設：false 隔行掃描gif進行渲染
-//       multipass: false // 類型：Boolean 預設：false 多次優化svg直到完全優化
-//     }))
-//     .pipe(gulp.dest('./public/img'))
-// })
+//pwa
+gulp.task('generate-service-worker', () => {
+  return workbox.injectManifest({
+    swSrc: './sw-template.js',
+    swDest: './public/sw.js',
+    globDirectory: './public',
+    globPatterns: [
+        "**/*.{html,css,js,json,woff2}"
+    ],
+    modifyURLPrefix: {
+        "": "./"
+    }
+  });
+});
 
 // 執行 gulp 命令時執行的任務
-gulp.task('default', gulp.parallel(
-  'compress', 'minify-css', 'minify-html'
-  // , 'minify-images'
-))
+gulp.task("default", gulp.series("generate-service-worker", gulp.parallel(
+    'compress','minify-html', 'minify-css'
+)));
---
+++ sw-template.js
@@ -0,0 +1,108 @@
+const workboxVersion = '5.1.4';
+
+importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
+
+workbox.core.setCacheNameDetails({
+    prefix: "CC的部落格"
+});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.precaching.cleanupOutdatedCaches();
+
+Images
+workbox.routing.registerRoute(
+    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "images",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// Fonts
+workbox.routing.registerRoute(
+    /\.(?:eot|ttf|woff|woff2)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "fonts",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// Google Fonts
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "google-fonts-stylesheets"
+    })
+);
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// Static Libraries
+workbox.routing.registerRoute(
+    /^https:\/\/cdn\.jsdelivr\.net/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "static-libs",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// External Images
+workbox.routing.registerRoute(
+    /^https:\/\/raw\.githubusercontent\.com\/reuixiy\/hugo-theme-meme\/master\/static\/icons\/.*/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "external-images",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+workbox.googleAnalytics.initialize();
