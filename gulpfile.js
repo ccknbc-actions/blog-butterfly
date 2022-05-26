a8025a8bd039abd6618e982b24d3e3eb118dc5cf
CCKNBC <ccknbc@qq.com>
2022-05-27 00:07:56 +0800

    👌 优化 SW 性能

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('generate-service-worker', () => {
     swSrc: './sw.js',
     swDest: './public/sw.js',
     globDirectory: './public',
-    globPatterns: ['index.html','**/*.{css,js}'],
+    globPatterns: ['{index,404}.html'],
     modifyURLPrefix: {
         '': './'
     }
