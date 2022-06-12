87a527c344fb0d8810e1818df42bc3eee15a0e71
CCKNBC <ccknbc@qq.com>
2022-06-12 21:32:07 +0800

    🐛 再次修复 Discuss 评论系统@lete114，暂时关闭PWA，测试一段时间SW

--- scaffolds/page.md
+++ scaffolds/page.md
@@ -4,7 +4,6 @@ translate_title:
 subtitle: 
 date: {{ date }}
 updated: {{ date }}
-keywords: 
 description: 
 top_img: false
 comments: true 
--- themes/butterfly/source/js/search/algolia.js
+++ themes/butterfly/source/js/search/algolia.js
@@ -92,13 +92,18 @@ window.addEventListener('load', () => {
     templates: {
       item(data) {
         const link = data.permalink ? data.permalink : (GLOBAL_CONFIG.root + data.path)
+        const result = data._highlightResult
+        const content = result.contentStripTruncate
+                        ? cutContent(result.contentStripTruncate.value)
+                        : result.contentStrip
+                        ? cutContent(result.contentStrip.value)
+                        : result.content
+                        ? cutContent(result.content.value)
+                        : ''
         return `
           <a href="${link}" class="algolia-hit-item-link">
-          ${data._highlightResult.title.value || 'no-title'}
-          <b>${data._highlightResult.title.value || 'no-title'}</b>
-          <p class="algolia-hit-item-content">${cutContent(data._highlightResult.contentStrip.value)}</p>
-          <p>匹配字词: <em><mark>${data._highlightResult.contentStrip.matchedWords}</mark></em> | 匹配等级: <em><mark>${data._highlightResult.contentStrip.matchLevel}</mark></em></p></a>
-          `
+          ${result.title.value || 'no-title'}
+          <p class="algolia-hit-item-content">${content}</p></a>`
       },
       empty: function (data) {
         return (
