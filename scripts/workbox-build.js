f0cba6187ce5c916087e54f2c991729f96a2adb2
CCKNBC <ccknbc@qq.com>
2022-12-30 23:20:34 +0800

    🌈 style(o): w

--- scripts/workbox-build.js
+++ scripts/workbox-build.js
@@ -2,7 +2,6 @@ const workboxBuild = require('workbox-build');
 
 hexo.on("deployBefore", async function () {
     const buildSW = () => {
-        // This will return a Promise
         return workboxBuild.injectManifest({
             swSrc: './workbox-sw.js',
             swDest: './public/sw.js',
@@ -12,7 +11,6 @@ hexo.on("deployBefore", async function () {
                 '': './'
             }
         }).then(({ count, size, warnings }) => {
-            //打印错误
             var size = (size/1024).toFixed(2)
             warnings.forEach(console.warn);
             hexo.log.info("已自动生成: sw.js");
