d1180683bb0c6935490e5bbb3348a28edb45227a
CC康纳百川 <ccknbc@qq.com>
2023-05-11 20:53:32 +0800

    Update Hexo Theme Butterfly DEV

--- source/js/custom.js
+++ source/js/custom.js
@@ -68,22 +68,3 @@ document.addEventListener("DOMContentLoaded", () => {
 //         div.children[0].style.maxHeight = (document.documentElement.clientHeight - 210) + 'px'
 //     })
 // }
-
-// 设置cookie
-function setCookie(cname,cvalue,exdays){
-    var d = new Date();
-    d.setTime(d.getTime()+(exdays*24*60*60*1000));
-    var expires = "expires="+d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
-}
-
-// 获取cookie
-function getCookie(cname){
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
-    }
-    return "";
-}
